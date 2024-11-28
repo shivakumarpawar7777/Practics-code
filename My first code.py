31eb8694fe590e1bf9e56aef5cada89f3632470b
shivakumarpawar7777 <shivupawar90@gmail.com>
2024-11-28 18:26:12 +0530

    added code

--- My first code.py
+++ My first code.py
@@ -1,1 +1,11 @@
 print("Hello world")
+print("----------------------")
+
+name = "My name is shiv"
+print(name)
+print("----------------------")
+
+name = input("Enter your word : ")
+age = input("Enter your word : ")
+
+print("----------------------")

b5e2459e27ae08651c40db2c5e6ae824e862eec3
shivakumarpawar7777 <shivupawar90@gmail.com>
2024-11-28 18:45:11 +0530

    arithmetic

---
+++ Syntax.py
@@ -0,0 +1,9 @@
+"""a = input("Enter your A value : ")
+b = input("Enter your B value : ")
+sum = float(a) + float(b) -1
+print(sum)"""
+
+a = input("Enter your A value : ")
+b = input("Enter your B value : ")
+sum = float(a) + float(b) -1
+print(sum)

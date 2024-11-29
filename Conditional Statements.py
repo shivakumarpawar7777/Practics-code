aaf39d82fa6c3fdb59c20fe8c8036f11dc28ed50
shivakumarpawar7777 <shivupawar90@gmail.com>
2024-11-29 13:49:25 +0530

    Conditional Statements:-if, elif, else

---
+++ Conditional Statements.py
@@ -0,0 +1,40 @@
+
+"""# if statement:- 
+z = int(input("Enter your number :- "))
+if z%2==0:
+    print("Its even number: ", z)  # We wil not get output. if conditinal not true.
+
+# else statement:- 
+z = int(input("Enter your number :- "))
+if z%2==0:
+    print("Its even number: ", z)
+else:
+    print("Its odd number: ", z)"""
+
+# elif    switchboard
+
+switchboard = "z"
+while True:
+    print("switchboard menu: ")
+    print("1. Ligth buttun")
+    print("2. Fan buttun")
+    print("3. TV buttun") 
+    print("4. Refrigerator") 
+    
+    option = int(input("Enter you option:- "))
+    if option == 1:
+        print("ligth is trun on")
+    elif option == 2: 
+        print("Fan is trun on")
+    elif option == 3:
+        print("TV is trun on")
+    elif option == 4:
+        print("Refrigerator is trun on")
+    else:
+        print("Invalid option - Please try again")
+
+
+
+         
+
+
---
+++ x.py
@@ -0,0 +1,9 @@
+str = ("I am shivakumar pawar")
+print(str.capitalize())
+print(str.upper())
+print(str.lower())
+print(str.replace("I am ", "My self "))
+print(str.startswith("I am"))
+print(str.endswith("war"))
+print(str.find("i"))
+print(str.count("a")) # it will find how many time word wil be other.

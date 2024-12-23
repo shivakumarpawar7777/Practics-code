d038146bc96da3b7e6bd9241ab0f8f0c67dfc474
shivakumarpawar7777 <shivupawar90@gmail.com>
2024-12-23 17:23:28 +0530

    add

---
+++ Python.py
@@ -0,0 +1,7 @@
+Python :- Python is interpreted, object oriented, high level programming language with dynamic semantics.
+
+#Feature of Python
+1. easy to learn & use.
+2. high level programming language. print("Hello world")
+3. Interpreted language. (reads the codes line by line)
+4. Dynamiclly type language 

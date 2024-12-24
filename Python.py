7987e854333932cb680060bdba98dbc2fd0a404c
shivakumarpawar7777 <shivupawar90@gmail.com>
2024-12-24 10:31:11 +0530

    add

--- Python.py
+++ Python.py
@@ -12,3 +12,27 @@
 9. Extensible.
 
 #  Histiry:-  Python  foundation by guido van rossum in 1980s & published on fabrury 1991.
+
+# Appliction of python 
+1. Web application or development 
+2. Data science
+3. Game development
+4. CAD (computer aided designs)
+5. Artificial intelligence & mechine learning
+6. Enterprise application
+7. Software development etc.
+
+# Advantages of pyton
+>>> Simple ane easy to learn
+>>> Readability 
+>>> langer community support 
+>>> extensive of libraies
+
+#Disadvatages of python
+>>> slow speed
+>>> Runtime errors
+>>> Poor memory efficiency or consumption
+>>> weak in mobile computing 
+>>> Database access
+
+

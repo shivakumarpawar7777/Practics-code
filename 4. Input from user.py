fd0b6b0fade744c50dbe600abd1f22f11c3b25a4
shivakumarpawar7777 <shivupawar90@gmail.com>
2024-12-25 16:07:32 +0530

    added

--- 4. Input from user.py
+++ 4. Input from user.py
@@ -1,3 +1,18 @@
+# Input() statement is used to accept values (using key board form user)
+"""name = input("Enter your name:- ")
+print("Hello", name)
+print(len(name))
+print(type(name))
+
 age = int(input("Enter your agr:- "))
 print(age>=18)
+print(type(age))
 
+age = int(input("Enter your agr:- "))
+print(age+1)
+print(type(age))
+
+mark = float(input("Enter your mark: "))
+print(mark>=35)
+print(mark+0.5)
+print(type(mark))"""

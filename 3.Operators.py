617de0e8a49c07e9625e865a7e4e57d2ffd92799
shivakumarpawar7777 <shivupawar90@gmail.com>
2024-12-25 11:32:20 +0530

    add

---
+++ 3.Operators.py
@@ -0,0 +1,18 @@
+#1. Arithmetic operators
+Addition "+" = 10+20=30
+subtraction  "="= 20-20=0
+Multiplication "*" = 2*2=4
+Division "/" = 20/2= 10
+Modulus "%" = 9%2=1
+Floor division "//"
+Exponention "**" = 2**4=16
+
+#2. Relation operator
+
+"==" = Two value are equal
+"=!" = Two value not equal 
+">"  = Greater then
+"<"  = Less then 
+"<=" = less then equal 
+">=" = Greater then equal 
+ 

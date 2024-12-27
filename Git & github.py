9cbe78d4830363f0028299f7bae5c1e9cf42e71f
shivakumarpawar7777 <shivupawar90@gmail.com>
2024-12-27 18:40:09 +0530

    add

--- Git & github.py
+++ Git & github.py
@@ -8,3 +8,16 @@ Git :- Version control system is a tools that help to track change in code
 >>> Track the history 
 >>> collaborat.
 
+#Git :-
+>>> Git --version
+>>> li
+>>> clear
+>>> pwd (working dierectry)
+
+# Git configuring 
+Git config --global user.name "my name"
+Git config --global user.email "my mail id"
+Git config --list
+
+
+

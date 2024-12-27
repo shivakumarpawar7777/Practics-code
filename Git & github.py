f7adaececbeb4ed232c834e22374c3c6bf34c242
shivakumarpawar7777 <shivupawar90@gmail.com>
2024-12-27 08:26:12 +0530

    git

---
+++ Git & github.py
@@ -0,0 +1,10 @@
+Git :- Version control system is a tools that help to track change in code 
+>>> Git is a varsion comtrol system 
+>>> Git --version 
+
+>>> It is popular
+>>> free & open source 
+>>> fast & scalable
+>>> Track the history 
+>>> collaborat.
+

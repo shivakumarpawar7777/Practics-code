0e25f9ab87cf2b5225c3346535b3f72636454d08
shivakumarpawar7777 <shivupawar90@gmail.com>
2024-12-27 18:56:38 +0530

    Git hub

--- Git & github.py
+++ Git & github.py
@@ -19,5 +19,20 @@ Git config --global user.name "my name"
 Git config --global user.email "my mail id"
 Git config --list
 
+# Git steps
+1. creat a account - git hub
+2. download a app - git hub 
+3. creating repository
+>>> git config --global user.name "Shivakumar"
+>>> git config --global user.mail "shiv@gmail.com"
+>>> git --global user.password "password"
 
-
+4. create folder in a tesktop 
+5. git clone (add a github file link)
+6. git init
+7. cd filename/   .(this is my file name in github or project name)
+8. code .  (vscode app)
+9. git add .
+10. git status
+11. git commit -m "any comment"
+12. git push origin main.

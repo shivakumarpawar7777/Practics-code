6bb702a44261fe678e4f30cc2c77e4ca5616e9cf
shivakumarpawar7777 <shivupawar90@gmail.com>
2025-01-03 13:47:03 +0530

    set

---
+++ 10. set in python.py
@@ -0,0 +1,11 @@
+"""set :- set is a sequence of charecters in between curly braces {1,2,3,4,5,6,7,8,9,0} not allows duplicate values 
+Ex:-
+s = {1,2,3,4,5,6,7,8,9,0,1,2,3,4}
+print(s)
+print(type(s))  #output :- {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}     <class 'set'>
+print("--------------")
+
+s = set ()
+s1 = {1,2,3,4,5,6,7,8,9,0,1,2,3,4}
+print(s, s1)
+print(type(s))"""

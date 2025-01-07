753b7bbff03f3f5635932f88b7b98d1d7d4d1227
shivakumarpawar7777 <shivupawar90@gmail.com>
2025-01-07 12:17:33 +0530

    aaa

--- 10. set in python.py
+++ 10. set in python.py
@@ -31,15 +31,8 @@ print(s)
 print("-----")
 #Updata()
 s = {1,2,3,4,5,6,7,8,9,0,1,2,3,4,"set",1,2.3,3,4, True}
-s1 = {"preveen", "rama", "shiva", "kishor"}
+s1 = {"preveen", "rama", "shiva", "kishor", "pawar"}
 s.update(s1)
-print(s)
-
-#removin element from the set
-#rmove()
-s = {1,2,3,4,5,6,7,8,9,0,1,2,3,4,"set",1,2.3,3,4, True,"preveen", "rama", "shiva", "kishor"}
-
-s.remove("preveen") # only one element can remove
 print(s)
 
 #removin element from the set
